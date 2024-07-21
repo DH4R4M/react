fc137d068011308ec768a39b5383058df370e965
Dharamvir Sinh <gohildharamvirsinh1530@gmail.com>
2024-07-21 21:07:49 +0530

    about

---
+++ day2/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello, my name is dharamvir sinh gohil. <br />
+      currently I pursuing full-stack web development. <br />
+    </div>
+  );
+}
+
+export default App;
